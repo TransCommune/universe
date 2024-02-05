3111dfd6f896df23167fd364ebba969b64fd2677
Aurelia <aurelia@acuteaura.net>
2024-02-05 10:10:05 +0100

    add aurelia to qemu-libvirtd, add keys

--- nixos-module/user/aurelia.nix
+++ nixos-module/user/aurelia.nix
@@ -3,7 +3,8 @@
     isNormalUser = true;
     description = "Aurelia";
     shell = pkgs.fish;
-    extraGroups = [ "wheel" ];
+    extraGroups = [ "wheel" "qemu-libvirtd" ];
+    openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJmjGIsSO9jE85xNPzzp0AWfOSXVL4qQ3cuXeKCvxe+q" ];
   };
   programs.fish.enable = true;
 }
