a39b4537566e8e859bcbbf37656762bc8fae0dd9
Cassandra <git@sapphicco.de>
2024-03-16 12:00:53 +0100

    users/nas: fix empty home directory

--- nixos-module/user/nas.nix
+++ nixos-module/user/nas.nix
@@ -3,15 +3,21 @@
   config,
   ...
 }: {
-  users.groups.nas.name = "nas";
+  users.groups.nas = {
+    gid = 992;
+    name = "nas";
+  };
   users.users.nas = {
     description = "NAS file owner and script runner";
 
+    uid = 994;
     name = "nas";
     group = config.users.groups.nas.name;
 
     isSystemUser = true;
+
     createHome = true;
+    home = "/home/nas";
 
     shell = pkgs.fish;
   };
