941a6d390c779c99e3cc5eef3f14dd70c12cddc4
Aurelia <git@acuteaura.net>
2024-04-17 12:04:41 +0200

    sunhome: use old initrd, add ssh

--- host/sunhome/default.nix
+++ host/sunhome/default.nix
@@ -34,9 +34,27 @@
 
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+
   boot.initrd.luks.devices."luks-root".device = "/dev/nvme0n1p2";
+  boot.initrd.network = {
+    enable = true;
+    ssh = {
+      enable = true;
+      port = 2222;
+      hostRSAKey = /boot/dropbear_rsa_host_key;
+      hostECDSAKey = /boot/dropbear_ecdsa_host_key;
+      hostDSSKey = /boot/dropbear_dss_host_key;
+      # this includes the ssh keys of all users in the wheel group, but you can just specify some keys manually
+      # authorizedKeys = [ "ssh-rsa ..." ];
+      authorizedKeys = with lib; concatLists (mapAttrsToList (name: user: if elem "wheel" user.extraGroups then user.openssh.authorizedKeys.keys else []) config.users.users);
+    };
+    postCommands = ''
+      echo 'cryptsetup-askpass' >> /root/.profile
+    '';
+  };
+
   boot.zfs.forceImportRoot = false;
-  boot.initrd.systemd.enable = true;
+  #boot.initrd.systemd.enable = true;
 
   # fix for crappy Intel 2.5GbE
   boot.kernelPackages = config.boot.zfs.package.latestCompatibleLinuxPackages;
