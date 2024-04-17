1ebc4dd19ccae14736ae5d4d2f985ddccb3e2d97
Aurelia <git@acuteaura.net>
2024-04-17 13:29:13 +0200

    Update hardware-configuration.nix

--- host/sunhome/hardware-configuration.nix
+++ host/sunhome/hardware-configuration.nix
@@ -35,7 +35,7 @@
 
   swapDevices = [ ];
 
-  hardware.enableAllFirmware = true;
+  hardware.enableRedistributableFirmware = true;
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
