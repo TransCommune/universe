f2d35ce5683132476798a0bd9f7f037f07391a35
Aurelia <git@acuteaura.net>
2025-01-19 15:02:01 +0100

    sunhome/nginx: add jellyfin back

--- host/sunhome/nginx.nix
+++ host/sunhome/nginx.nix
@@ -64,6 +64,18 @@
       };
     };
 
+    virtualHosts."jellyfin.nullvoid.space" = {
+      addSSL = true;
+      enableACME = true;
+      locations."/" = {
+        proxyPass = "http://127.0.0.1:8096";
+        proxyWebsockets = true;
+        extraConfig = ''
+          client_max_body_size 8G;
+        '';
+      };
+    };
+
     virtualHosts."89.1.7.228" = {
       rejectSSL = true;
       default = true;
