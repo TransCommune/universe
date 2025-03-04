fe03a0a50aa4cdf4a6a6731be0e0d0c542bf5c30
Aurelia <git@acuteaura.net>
2025-03-04 11:35:19 +0100

    sunhome(attic): path?

--- host/sunhome/attic.nix
+++ host/sunhome/attic.nix
@@ -5,7 +5,7 @@
       listen = "127.0.0.1:49152";
       allowed-hosts = ["attic.nullvoid.space"];
       api-endpoint = "https://attic.nullvoid.space/";
-      path = "/magpie/apps/attic/data";
+      storage.path = "/magpie/apps/attic/data";
     };
     environmentFile = "/etc/attic.env";
   };
