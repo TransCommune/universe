34f77a612039160d5c4f004ee4ddcacc4036c697
Aurelia <git@acuteaura.net>
2025-05-19 11:31:32 +0200

    user(aurelia): trusted by nix:tm:

--- nixos-module/user/aurelia.nix
+++ nixos-module/user/aurelia.nix
@@ -7,4 +7,5 @@
     openssh.authorizedKeys.keys = ["ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJmjGIsSO9jE85xNPzzp0AWfOSXVL4qQ3cuXeKCvxe+q"];
   };
   programs.fish.enable = true;
+  nix.settings.trusted-users = ["aurelia"];
 }
