9ef4c7c42216cda0caa3c0b357a7f8bdcbb13cc8
Aurelia <git@acuteaura.net>
2025-05-19 11:31:53 +0200

    users(sapphiccode): trusted by nix:tm:

--- nixos-module/user/cassandra.nix
+++ nixos-module/user/cassandra.nix
@@ -7,4 +7,5 @@
     initialHashedPassword = "$y$j9T$hmGf7F7DO5k6E9IbH/dTN.$gvp63YhpxwQrbGCfoRgGw7NGdUplN2gRqbsO5ANDSo3";
   };
   programs.fish.enable = true;
+  nix.settings.trusted-users = ["sapphiccode"];
 }
