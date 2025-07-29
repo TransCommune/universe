c7de4777708ecb83e4aa783dbb39e4ebb1a01712
Cassandra <git@sapphicco.de>
2025-07-29 18:40:01 +0200

    feat: add copyparty

---
+++ host/sunhome/container/copyparty.nix
@@ -0,0 +1,41 @@
+{config, ...}: {
+  environment.etc = {
+    "copyparty.d/mounts.cfg" = {
+      text = ''
+        [global]
+          e2ds  # index files on startup and upload
+          e2ts  # scan files for media tags on startup and upload
+
+        [/media]
+          /mnt/media
+          accs:
+            r: *
+      '';
+      mode = "0644";
+    };
+  };
+
+  virtualisation.quadlet.containers.copyparty = {
+    containerConfig = {
+      name = "copyparty";
+      image = "ghcr.io/9001/copyparty/ac:latest";
+      volumes = [
+        "/etc/copyparty.d:/cfg:U"
+        "/magpie/media:/mnt/media"
+      ];
+      networks = ["copyparty.network"];
+      publishPorts = [
+        "3923:3923/tcp"
+      ];
+      user = config.users.users.nas.uid;
+      group = config.users.groups.nas.gid;
+    };
+    unitConfig = {
+      After = ["magpie.target"];
+      Wants = ["magpie.target"];
+      RequiresMountsFor = [
+        "/magpie/media"
+      ];
+    };
+  };
+}
